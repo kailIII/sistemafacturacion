9d756ea98be754b4ed51a36617bc69c45116917e
Santiago Benitez E <sbenitez.sbe@gmail.com>
2017-05-18 08:54:33 -0500

    Se configuro las rutas en symfony 3 y angular 2

--- front/app/app.module.ts
+++ front/app/app.module.ts
@@ -1,11 +1,19 @@
-import { NgModule }      from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
- 
+import { routing, appRoutingProviders } from './app.routing';
+
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+	imports:      [ 
+				BrowserModule,
+				routing
+					],
+	declarations: [ 
+				AppComponent 
+					],
+	providers:    [ appRoutingProviders],
+	bootstrap:    [ AppComponent ],
+	schemas: [CUSTOM_ELEMENTS_SCHEMA ]
 })
- 
+
 export class AppModule { }
